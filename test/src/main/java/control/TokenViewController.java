4139c8d6dacc4cea247dca92e1ed558c7d231fd7
hanhao <535528121@qq.com>
2019-01-16 15:18:33 +0800

    灵活增加查询条件

--- test/src/main/java/control/TokenViewController.java
+++ test/src/main/java/control/TokenViewController.java
@@ -8,6 +8,7 @@ import com.jfinal.core.Controller;
 public class TokenViewController extends Controller {
 	
 	public void getTokenView() {
+		String secuiteKey = getPara("excoin");
 		java.util.Date dateNowDate = new java.util.Date();
     	String timeStr13 = dateNowDate.getTime() + "";
     	String time13Substring = timeStr13.substring(0, 10) + "." + timeStr13.substring(10,13);
@@ -15,7 +16,7 @@ public class TokenViewController extends Controller {
     	String timeStr13Before4 = (dateNowDate.getTime() - (4 * 24 * 60 * 60 * 1000)) + "";
     	String time13SubstrBefore4 = timeStr13Before4.substring(0, 10) + "." + timeStr13Before4.substring(10,13);
 
-    	String url = "https://tokenview.com/v2api/trading/dots/eth/"+ time13SubstrBefore4 +"/?endTime=" + time13Substring;
+    	String url = "https://tokenview.com/v2api/trading/dots/"+ secuiteKey +"/"+ time13SubstrBefore4 +"/?endTime=" + time13Substring;
         String jsonStr = "{xxx}";
         String httpOrgCreateTestRtn = HttpUtilCSDN.doGet(url);
         renderJson(JSON.parseObject(httpOrgCreateTestRtn));
